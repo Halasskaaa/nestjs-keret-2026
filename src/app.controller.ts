c758d2787fb8298d7b648dde0e4353c12502a737
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2026-09-23 11:30:42 +0200

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service.js';
 import { Expense } from './expense.js';
-
+ 
 @Controller()
-export class AppController { 
-      expenses: Expense[] = [
+export class AppController {
+   expenses: Expense[] = [
       {
         name: 'Élelmiszer',
         amount: 12500,
@@ -56,30 +56,37 @@ export class AppController {
         category: 'utilities',
       },
     ];
-
+ 
   constructor(private readonly appService: AppService) {}
-  
+ 
   @Get()
   @Render('index')
   getHome() {
     const total = this.expenses.reduce((sum, expense) => sum + expense.amount, 0);
     return {
-      title: 'Összes kiadás összege',
-      total
+      title: 'Összes kiadás összege', total
+    };
+  }
+ 
+  @Get('all')
+  @Render('all')
+  getAll(){
+    return{
+      title: 'Összes kiadás',
+      expenses: this.expenses
+    };
+  }
+ 
+  @Get('top3')
+  @Render('all')
+  getTop3(){
+    const top3 = [...this.expenses]
+    .sort((a, b) => b.amount - a.amount)
+    .slice(0, 3);
+ 
+    return{
+      title: 'Top 3 kiadás',
+      expenses : top3
     }
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
